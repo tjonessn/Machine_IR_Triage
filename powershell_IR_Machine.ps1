4c76682faf3faacbac49aa3a7b6c462ec8a3df32
tjonessn <76185838+tjonessn@users.noreply.github.com>
2021-07-06 14:28:52 -0500

    Update powershell_IR_Machine.ps1

--- powershell_IR_Machine.ps1
+++ powershell_IR_Machine.ps1
@@ -1,13 +1,13 @@
-#checks to make sure file is being run as elevated
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
     { 
         Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit 
     }
 
+
 #create directory to store all the outputted files at C:\StratNet_Triage
 New-Item -Path "c:\" -Name "StratNet_Triage" -ItemType "directory"
 
-#execution poliocy might have been changed for remote code execution purposes
+#execution poliocy might have been changed
 
 Get-ExecutionPolicy | Out-File -FilePath C:\StratNet_Triage\Execution_Policy.txt
 
@@ -15,7 +15,6 @@ Get-ExecutionPolicy | Out-File -FilePath C:\StratNet_Triage\Execution_Policy.txt
 New-Item -Path "c:\StratNet_Triage" -Name "Group_and_User_info" -ItemType "directory"
 Get-LocalUser | Export-Csv -Path C:\StratNet_Triage\Group_and_User_info\Local_Users.csv
 Get-LocalGroup | Export-Csv -Path C:\StratNet_Triage\Group_and_User_info\Local_Groups.csv
-Get-ADUser -Filter * -Properties * | Export-Csv -Path C:\StratNet_Triage\Group_and_User_info\AD_Users.csv -NoTypeInformation
 
 #get all users in all groups
 $groups = Get-LocalGroup
@@ -27,7 +26,7 @@ foreach ($group in $groups)
 #networks, open ports, and adapter info
 New-Item -Path "c:\StratNet_Triage" -Name "Network_Info" -ItemType "directory"
 Get-NetIPConfiguration -All | Out-File -FilePath C:\StratNet_Triage\Network_Info\IP_Config.txt
-Get-NetTCPConnection | Out-File -FilePath C:\StratNet_Triage\Network_Info\NetStat_info.txt
+netstat -naob| Export-Csv -Path C:\StratNet_Triage\Network_Info\NetStat_info.txt
 Get-NetRoute | Out-File -FilePath C:\StratNet_Triage\Network_Info\Route_Info.txt
 netsh wlan show profiles | Out-File -FilePath C:\StratNet_Triage\Network_Info\wifi_networks.txt
 arp -a | Out-File -FilePath C:\StratNet_Triage\Network_Info\ARP_Info.txt
@@ -45,14 +44,13 @@ Get-ComputerInfo | Out-File -FilePath C:\StratNet_Triage\System_Info.txt
 #installed apps
 Get-CimInstance -ClassName Win32_Product | Export-Csv -Path C:\StratNet_Triage\Installed_Apps.csv
 
-#eventlog
+#eventlog - get evtx files for analysis with https://github.com/sans-blue-team/DeepBlueCLI or other manual analysis
 New-Item -Path "c:\StratNet_Triage" -Name "Event_Logs" -ItemType "directory"
-$now = Get-Date
-Get-EventLog -LogName System -Before $now | Export-Csv -Path C:\StratNet_Triage\Event_Logs\EventLog_System.csv
-Get-EventLog -LogName Application -Before $now | Export-Csv -Path C:\StratNet_Triage\Event_Logs\EventLog_Application.csv
-Get-EventLog -LogName Security -Before $now | Export-Csv -Path C:\StratNet_Triage\Event_Logs\EventLog_Security.csv
-Get-WinEvent -Logname Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational | Export-Csv -Path C:\StratNet_Triage\Event_Logs\RDP_logs.csv
-Get-WinEvent 'Microsoft-Windows-Windows Defender/Operational' | Export-Csv -Path C:\StratNet_Triage\Event_Logs\WinDefender.csv
+Copy-Item -Path "C:\Windows\System32\winevt\Logs\" -Destination C:\StratNet_Triage\Event_Logs -Recurse
+
+#get srum logs for later analysis with https://github.com/MarkBaggett/srum-dump or other tool
+New-Item -Path "c:\StratNet_Triage" -Name "srum_dump" -ItemType "directory"
+Copy-Item -Path "C:\Windows\System32\sru\" -Destination C:\StratNet_Triage\srum_dump -Recurse
 
 #memory dump 
 New-Item -Path "c:\StratNet_Triage" -Name "Memory_Dumps" -ItemType "directory"
@@ -60,12 +58,11 @@ New-Item -Path "c:\StratNet_Triage" -Name "Memory_Dumps" -ItemType "directory"
 #use comae dumpit.exe tool, must be in same directory as this ps1 file
 if ([System.Environment]::Is64BitOperatingSystem -eq $true) 
 {
-    C:\Offline_Machine_IR_Triage\dumpit\x64\DumpIt.exe /N /Q /O C:\StratNet_Triage\Memory_Dumps\memdump.dmp
+    C:\Machine_IR_Triage\dumpit\x64\DumpIt.exe /N /Q /O C:\StratNet_Triage\Memory_Dumps\memdump.dmp
 }
 else
 {
-    C:\Offline_Machine_IR_Triage\dumpit\x86\DumpIt.exe /N /Q /O C:\StratNet_Triage\Memory_Dumps\memdump.dmp
+    C:\Machine_IR_Triage\dumpit\x86\DumpIt.exe /N /Q /O C:\StratNet_Triage\Memory_Dumps\memdump.dmp
 }
 
 Pause
-
